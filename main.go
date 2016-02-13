f804f16444e9f50d995777d9289a570077910a45
Kevin McDonald <k3vinmcdonald@gmail.com>
2016-02-13 12:58:26 -0600

    Removes timezone (just rely on locale settings), adds example systemd service file

--- main.go
+++ main.go
@@ -12,20 +12,13 @@ import (
 func main() {
 	var (
 		addr           string
-		timezone       string
 		textNumbersStr string
 		textNumbers    []string
 	)
 
 	flag.StringVar(&addr, "addr", ":3000", "address to listen to (default is 0.0.0.0:3000)")
-	flag.StringVar(&timezone, "tz", "UTC", "timezone (https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)")
 	flag.StringVar(&textNumbersStr, "text", "", "comma-separated numbers to text")
 	flag.Parse()
-
-	location, err := time.LoadLocation(timezone)
-	if err != nil {
-		log.Fatal(err)
-	}
 
 	if len(textNumbersStr) > 0 {
 		textNumbers = strings.Split(textNumbersStr, ",")
@@ -44,12 +37,13 @@ func main() {
     <Pause length="1" />
     <Play digits="6" />
     `)
-		message := fmt.Sprintf("Someone has arrived at your building at %s.", time.Now().In(location).Format(time.Kitchen))
+		message := fmt.Sprintf("Someone has arrived at your building at %s.", time.Now().Format(time.Kitchen))
 		for _, number := range textNumbers {
 			fmt.Fprintf(w, `<Sms to="+%s">%s</Sms>`, number, message)
 		}
 		w.Write([]byte("</Response>"))
 	})
 
-	http.ListenAndServe(":3000", nil)
+	log.Printf("Starting tele-entry server listening on %s", addr)
+	log.Fatal(http.ListenAndServe(addr, nil))
 }
